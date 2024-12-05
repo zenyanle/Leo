ef04e777952ebda42af9a6a1ef7b7fda3547bc61
zenyanle <zenyanle.vip@gmail.com>
2024-12-05 17:27:58 +0800

    some

---
+++ app/types/schema.ts
@@ -0,0 +1,21 @@
+// types/schema.ts
+export interface BlogPost {
+    id: string;
+    cover: string;
+    title: string;
+    tags: Array<{ name: string; color: string }>;
+    description: string;
+    date: string;
+    slug: string;
+}
+
+export interface PostPage {
+    post: BlogPost;
+    markdown: { parent: string }; // 修改这里以匹配 notion-to-md 的返回类型
+}
+
+export interface FullBlogData {
+    posts: PostPage[];
+    tags: string[];
+    totalPosts: number;
+}
